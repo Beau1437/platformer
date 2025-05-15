0b25fb162c4d22c43c31634ecb31f68ed71637f4
Beau Hamel <beauhamel@hotmail.com>
2025-05-15 09:18:20 -0700

    shotgun

--- src/player/gun.gd
+++ src/player/gun.gd
@@ -6,7 +6,11 @@ func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("shoot"):
-		var bullet_instance = bullet.instantiate()
-		get_tree().root.add_child(bullet_instance)
-		bullet_instance.global_position = global_position
-		bullet_instance.rotation = rotation
+		for n in 5:
+			var bullet_instance = bullet.instantiate()
+			get_tree().root.add_child(bullet_instance)
+			bullet_instance.global_position = global_position
+			if randf_range(1,2) > 1.5:
+				bullet_instance.rotation = rotation + randf_range(0, 0.25)
+			else:
+				bullet_instance.rotation = rotation - randf_range(0, 0.25)
